534c28f8e3a76c1e3c70a8c28495c12c245725bb
Asiphe04 <asiphendimlana40@gmail.com>
2023-08-14 16:23:47 +0200

    ..

--- server.js
+++ server.js
@@ -2,27 +2,25 @@ const express = require("express");
 const app = express();
 const users = [];
 
-app.set('view-engine', 'ejs');
-app.use(express.urlencoded({extended: false}));
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-   res.render('index.ejs', {firstName: "Asiphe"}) 
+  res.render("index.ejs", { firstName: "Asiphe" });
 });
 
-app.get('/login', (req, res) => {
-    res.render('login.ejs')
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
 });
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {});
 
+app.get("/signUp", (req, res) => {
+  res.render("signUp.ejs");
 });
 
-app.get('/signUp', (req, res) => {
-    res.render('signUp.ejs')
-});
-
-app.post('/signUp', (req, res) => {
-req.body.name
+app.post("/signUp", (req, res) => {
+  req.body.name;
 });
 
 app.listen(1111);
